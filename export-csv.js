71c2841f46263d608ee636195a20a123cc5baf17
Jafar Kamarudeen <kamsy74@gmail.com>
2023-02-28 19:29:14 +0100

    update file

--- export-csv.js
+++ export-csv.js
@@ -1,17 +1,9 @@
 async function exportCSV() {
-    this.exportLoading = true;
-
-    try {
-        let exportData = null;
-        if (this.checkedDataEnrichments.length) {
-            exportData = {
-                rows: this.checkedDataEnrichments
-            };
-        }
-        const response = await this.exportDataEnrichmentsHistory(exportData);
+    
+        const data = 'xxx';
 
         let csvContent = 'data:text/csv;charset=utf-8,';
-        let csvData = new Blob([response.data], {
+        let csvData = new Blob([data], {
             type: csvContent
         });
 
@@ -21,14 +13,5 @@ async function exportCSV() {
         link.setAttribute('href', csvUrl);
         link.setAttribute('download', 'export.csv');
         link.click();
-    } catch (error) {
-        this.showAlert({
-            status: 'error',
-            message: error.response.data.message,
-            showAlert: true
-        });
-    } finally {
-        this.exportLoading = false;
-        this.checkedDataEnrichmentss = [];
-    }
+     
 }
